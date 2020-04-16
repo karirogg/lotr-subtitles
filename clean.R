d803445c2f5e900b347bee3f5a7f84dfc8d310a4
karirogg <karirogg@gmail.com>
2020-04-16 20:19:16 +0000

    framelist written to file frames.csv

--- clean.R
+++ clean.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 #movies=c("fotr2.srt", "ttt2.srt", "rotk2.srt")
 movies=c("ttt2.srt")
-dat = data.frame(movie=c(), startTime=c(), endTime=c(), text=c(), stringsAsFactors = F)
+dat = data.frame(movie=c(), startFrame=c(), endFrame=c(), text=c(), stringsAsFactors = F)
 
 for(movie in movies) {
     currentString = ""
@@ -12,12 +12,12 @@ for(movie in movies) {
     subtitles = unlist(strsplit(temp, split="\n"))
     for(subtitle in subtitles) {
         if(grepl("-->", subtitle)) {
-            if(currentString != "") {
+            if(currentString != "" && currentTime != "") {
                 currentString=trimws(tolower(gsub(pattern = '\\d|<i>|</i>|\\.|:|,|\\r|\\?|\\!|\\-|"|\\(|\\)',"", currentString)))
                 currentTime=gsub(pattern = "\\r","", currentTime)
                 splitTime = unlist(strsplit(currentTime, " "))[c(1,3)] %>% hms(.) %>% period_to_seconds()
-                splitFrames = c(floor(splitTime[1]*24))
-                dat = rbind(dat, data.frame(movie=movie, startTime=splitTime[1], endTime=splitTime[2], text=currentString, stringsAsFactors = F))
+                splitFrames = c(floor(splitTime[1]*24),ceiling(splitTime[2]*24))
+                dat = rbind(dat, data.frame(movie=movie, startFrame=splitFrames[1], endFrame=splitFrames[2], text=currentString, stringsAsFactors = F))
                 currentString = ""
             } 
             currentTime = subtitle
@@ -29,15 +29,12 @@ for(movie in movies) {
     }
 }
 
-
-dat = dat %>% 
-    separate_rows(., text, sep=" ") %>%
-    mutate(nwords=1)
+dat = dat %>% separate_rows(., text, sep=" ") %>% mutate(nwords=1)
 
 tmp_dat <- dat %>%
     mutate(text2=c(text[2:nrow(.)],""),
-           checkTime=c(startTime[2:nrow(.)],""))%>%
-    filter(checkTime==startTime)
+           checkTime=c(startFrame[2:nrow(.)],""))%>%
+    filter(checkTime==startFrame)
 
 while(any(tmp_dat$text2!="")){
     tmp_dat <- tmp_dat %>%
@@ -45,7 +42,7 @@ while(any(tmp_dat$text2!="")){
                nwords=max(dat$nwords)+1)
         
     
-    dat <- bind_rows(dat,select(tmp_dat,movie,startTime,endTime,text,nwords))
+    dat <- bind_rows(dat,select(tmp_dat,movie,startFrame,endFrame,text,nwords))
     
     tmp_dat <- tmp_dat %>%
         mutate(text2=c(text2[2:nrow(.)],""),
@@ -58,8 +55,8 @@ lyricsdat = data.frame(songname=c(), nword=c(), word=c(), stringsAsFactors = F)
 
 for (song in songs) {
     rawlyr = readChar(song, file.info(song)$size)
-    subbedlyr = rawlyr %>% sub("\\[[0-9][0-9]:[0-9][0-9].[0-9][0-9]\\]","á",.) %>% gsub("\\[|\\]", " ", .) %>% gsub("\\.|\\,|\\?|\\!|\\n|\\:|\\-|\\d|\\(|\\)", "", .) %>% tolower()
-    templyr = unlist(strsplit(subbedlyr, split="á"))
+    subbedlyr = rawlyr %>% sub("\\[[0-9][0-9]:[0-9][0-9].[0-9][0-9]\\]","\303\241",.) %>% gsub("\\[|\\]", " ", .) %>% gsub("\\.|\\,|\\?|\\!|\\n|\\:|\\-|\\d|\\(|\\)", "", .) %>% tolower()
+    templyr = unlist(strsplit(subbedlyr, split="\303\241"))
     splitlyr = unlist(strsplit(templyr[2], split=" "))
     splitlyr = splitlyr[splitlyr != ""]
     lyricsdat = rbind(lyricsdat, data.frame(songname=song, nword=1:length(splitlyr), word=splitlyr, stringsAsFactors = F))
@@ -67,25 +64,31 @@ for (song in songs) {
 
 totaldat = left_join(lyricsdat, dat, by=c("word"="text"))
 
-uniquematch = group_by(totaldat, songname, nword, word) %>% summarize(movie = movie[1], startTime=startTime[1], endTime=endTime[1]) %>% ungroup()
+uniquematch = group_by(totaldat, songname, nword, word) %>% summarize(movie = movie[1], startFrame=startFrame[1], endFrame=endFrame[1]) %>% ungroup()
 uniquematchrandom = group_by(totaldat, songname, nword, word) %>% sample_n(size = 1) %>% ungroup()
 matches = inner_join(uniquematch, uniquematchrandom, by=c("songname", "nword", "word"), suffix=c("","_random"))
 
 group_by(uniquematch, songname) %>% summarize(sum(!is.na(movie))/n())
 
-#meikar meiri sens að skila frames að mínu mati þar sem sekúndur passa ekki endilega við frame timestampinn
+#meikar meiri sens ad skila frames ad minu mati thar sem sekundur passa ekki endilega vid frame timestampinn
+#skilum vector af frames sem python scriptid loopar i gegnum
+survive_unique_matches = uniquematchrandom %>% filter(., songname == "survive.lrc")
 
+shartmixdat =  bind_rows(
+  (filter(dat, grepl("pet", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 8)),
+  (filter(dat, text == "ring") %>% slice(1) %>% rename(word = text) %>% mutate(nword = 8)),
+  (filter(dat, grepl("fried", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 8)),
+  (filter(dat, grepl("pace", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 50)),
+  (filter(dat, grepl("o'clock", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 72)),
+  (filter(dat, grepl("brother", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 93)),
+  (filter(dat, grepl("crumble", text)) %>% slice(1) %>% rename(word = text) %>% mutate(nword = 127)),
+  (filter(dat, text == "because") %>% slice(1)))
+
+frameVec = c()
+
+for(item in survive)
 
 write.table(matches, file="matches.tsv", sep="\t", row.names = F, quote = F)
-
-filter(dat, grepl("pet", text))
-filter(dat, text == "ring")
-filter(dat, grepl("ied", text))
-filter(dat, grepl("pace", text))
-filter(dat, grepl("o'clock", text))
-filter(dat, grepl("brother", text))
-filter(dat, grepl("crumble", text)) 
-filter(dat, text == "because") %>% slice(1)
 
 group_by(dat, text) %>% summarize(count = n()) %>% arrange(desc(count)) %>% View
 
