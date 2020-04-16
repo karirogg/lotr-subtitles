5e166a1b977a3faedfb495a5efa14596e63a7b26
karirogg <karirogg@gmail.com>
2020-04-16 23:36:47 +0000

    wrong file replaced

--- movie_clip_editor.py
+++ movie_clip_editor.py
@@ -1,30 +1,24 @@
 from moviepy.editor import *
 import pandas as pd
 
-matching_dat=pd.read_csv('matches.tsv',sep='\t',header=0)
+matching_dat=pd.read_csv('survive_frames.tsv',sep='\t',header=0)
 survive_matchin_dat=matching_dat[matching_dat['songname']=='survive.lrc'][matching_dat['nword']<=176]
-ttt_movie=VideoFileClip("/Users/solviro/Downloads/ttt_movie.mp4")
 
-count=0
-tmp_time_stamp=
-tmp_images=[]
-for timestamp, raw_img in ttt_movie.iter_frames(with_times=True):
-    count=count+1
-    if count>2900 and count<=3000:
-        tmp_images.append(ImageClip(raw_img).set_duration(1.0/24))
-    elif count>3000:
-        break
-concat_clip = concatenate_videoclips(tmp_images, method="compose")
-concat_clip.write_videofile("test.mp4", fps=24)
+survive_dat=pd.read_csv('survive_frames.tsv',sep='\t',header=0)
+ttt_movie=VideoFileClip("/Users/bollo/Documents/Kari/Forritun/ttt_movie.mp4") 
+fotr_movie = 0
+rotk_movie = 0
 
-times=[seq(ts1start,ts1end,by=1.0/24),seq(ts2start,ts2end),...,seq(ts2start,ts2end)]
-out_video=[]
-out_audio=[]
-for(t in times):
-    out_video.append(ImageClip(ttt_movie.get_frame(t)).set_duration(1.0/24))
+out_clip = ttt_movie.subclip(0,1)
+for i in range(survive_dat.shape[0]):
+    movie = ttt_movie
+    if survive_dat['movie'][i] == 'fotr2.srt':
+        movie = fotr_movie
+    elif survive_dat['movie'][i] == 'ttt2.srt':
+        movie = ttt_movie
+    elif survive_dat['movie'][i] == 'rotk2.srt':
+        movie = rotk_movie
+    current_clip = movie.subclip(survive_dat["startTime"][i], survive_dat["endTime"][i])
+    out_clip = concatenate_videoclips([out_clip, current_clip])
 
-for(t in times):
-    out_audio.append(AudioClip(ttt_movie.audio.get_frame(t)).set_duration(1.0/24))
-
-concat_clip = concatenate_videoclips(out_video, method="compose")
-concat_clip.write_videofile("survive_edited.mp4", fps=24)
+out_clip.write_videofile("survive_edited_full1.mp4", codec='libx264')
